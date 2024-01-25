f8d92350b2fade86a5626b7a7e4bdbe96dba6e01
Davidson-09 <davidsonakra@gmail.com>
2024-01-25 18:29:41 +0100

    extra changes

--- contracts/SinglePlayer.sol
+++ contracts/SinglePlayer.sol
@@ -3,21 +3,13 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./WordSelector.sol";
-import "@api3/airnode-protocol/contracts/rrp/requesters/RrpRequesterV0.sol";
 
-contract WordanaGame is RrpRequesterV0, wordSelector  {
+contract WordanaGame is  wordSelector  {
     // Wordana token contract
     IERC20 public wordanaToken;
     address public owner;
     string private wordOfTheDay;
     uint256 public tokensToEarn;
-
-    // params for random number generator (API3 QRNG)
-    address public airnode;
-    bytes32 public endpointIdUint256;
-    address public sponsorWallet;
-    mapping (bytes32 => bool) public  expectingRequestWithIdToBeFulfilled;
-    uint256 public sindex;
     string private appkey;
 
 
@@ -45,7 +37,7 @@ contract WordanaGame is RrpRequesterV0, wordSelector  {
     }
 
     // Start the game and set the Wordana token contract
-    constructor(address _wordanaToken, uint256 _tokensToEarn, address _airnodeRrp, string memory _appkey) RrpRequesterV0(_airnodeRrp) {
+    constructor(address _wordanaToken, uint256 _tokensToEarn, string memory _appkey) {
         require(_wordanaToken != address(0), "Invalid Wordana token address");
         owner = msg.sender;
         wordanaToken = IERC20(_wordanaToken);
@@ -56,29 +48,6 @@ contract WordanaGame is RrpRequesterV0, wordSelector  {
 
     function setReward(uint256 _tokensToEarn) public onlyOwner {
         tokensToEarn = _tokensToEarn*1000000000000000000;
-    }
-
-    function setRandomNumberRequestParameters(address _airnode,
-     bytes32 _endpointIdUint256, 
-     address _sponsorWallet) external onlyOwner {
-        airnode = _airnode;
-        endpointIdUint256 = _endpointIdUint256;
-        sponsorWallet = _sponsorWallet;
-    }
-
-    function makeRequestUint256() public onlyOwner  {
-        bytes32 requestId = airnodeRrp.makeFullRequest(
-            airnode,
-            endpointIdUint256,
-            address(this),
-            sponsorWallet,
-            address(this),
-            this.selectWord.selector,
-            ""
-        );
-
-        expectingRequestWithIdToBeFulfilled[requestId] = true;
-        emit RequestUint256(requestId);
     }
 
     // Player guesses the word
@@ -112,17 +81,6 @@ contract WordanaGame is RrpRequesterV0, wordSelector  {
     function setWordOfTheDay(uint256 index) public onlyOwner {
         // will use api3qrng
         wordOfTheDay = getWord(index);
-        emit wordOfTheDaySet();
-    }
-
-    function selectWord(bytes32 requestId, bytes calldata data) external onlyAirnodeRrp {
-        require(expectingRequestWithIdToBeFulfilled[requestId], "Request id unknown");
-        expectingRequestWithIdToBeFulfilled[requestId] = false;
-        uint256 randomNumber = abi.decode(data, (uint256));
-        // the index should be in the range of 0 - 260
-        uint256 wordIndex = (randomNumber % (260 - 0 + 1)) + 0;
-        sindex = wordIndex;
-        setWordOfTheDay(wordIndex);
         emit wordOfTheDaySet();
     }
 
